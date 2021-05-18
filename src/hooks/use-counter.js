66173add19391b33a050834bcada13199498e0a7
Veronika Ozretic <vozret00@fesb.hr>
2021-05-18 10:31:58 +0200

    configuring hook for backwardsCounter (188)

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,15 +1,21 @@
 import { useState, useEffect } from "react";
 
-const useCounter = () => {
+// forwards is true if we want to add and false if we want to subtract
+// optional parameter
+const useCounter = (forwards = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwards]);
 
   return counter;
 };
